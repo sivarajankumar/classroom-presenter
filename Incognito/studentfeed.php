02efbea674ca20b590373a76f8e3742771731700
shenama smd <shenama.smd@gmail.com>
2011-04-29 15:35:26 -0700

    Chris's changes ----take 3

--- Incognito/studentfeed.php
+++ Incognito/studentfeed.php
@@ -3,33 +3,47 @@
 
 	<head>
 		<title>Incognito</title>
-
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="pages.css" type="text/css" rel="stylesheet" />
 	</head>
 
 	<body>
 		
-		<div id="topbanner"> 
-			
+		<div id="topbanner"> 	<!-- Includes logo & person's information/help/logout, & course name -->		
 			<img src="logo.png" alt="logo" />
 			<div id="greeting">
-				Hello [student's alias]! | <a class="aboutlink" href="help.php">Help</a> | <a href="login.php">Logout</a> <br />
-				You are currently viewing ______.
+				Hello [student's alias]! | <a href="studentsettings.php">Your Settings</a> |  <a class="aboutlink" href="help.php">Help</a> | <a href="login.php">Logout</a> <br />
+				You are currently looking at [course name].
 			</div>
 		</div>			
 
 		
-		<div id="navigation">
+		<div id="navigation"> <!-- Navigation bar -->
 			<ul>
 				<li><span><a class="tab" href="studenthome.php">Home</a></span></li>
 				<li><span><a class="tab" href="studentfeed.php">Feed</a></span></li>
-				<li><span><a class="tab" href="surveys.php">Surveys</a></span></li>
+				<li><span><a class="tab" href="studentsurveys.php">Surveys</a></span></li>
 			</ul>		
 		</div>
 		
 		<div id = "maincontent">
-			<div id="filterandsort">	
+			<form method="post">
+			
+				<div class="submissioncontent">	<!-- Includes: "Submit as", textbox, & submit button -->
+					<div id="typeAreaFeed">
+						<span>Submit as:
+							<label><input type="radio" name="submitType" value="Q" checked="checked"/> Question</label>
+							<label><input type="radio" name="submitType" value="F"/> Feedback </label>
+						</span> <br />
+						<textarea name="textfeed" rows="2" cols="70">
+						</textarea>					
+						<button type="submit" id="submitbutton">Submit</button>
+					</div>
+				</div>
+				
+			</form>
+			
+			<div id="filterandsort">	<!-- Filtering & Sorting -->
 				<span>
 					FILTER BY: 
 					<select name="filter">
@@ -48,30 +62,30 @@
 					</select>
 				</span>
 				
-				<span>
-					SORT BY:
+				<span> SORT BY:
 					<a href="" >NEWEST</a> | <a href="" >HIGHEST PRIORITY</a>
 				</span>				  
 			</div>
 			
-			<div id="feedbox">
-				<div id="votes"> Vote?
+			
+			<div id="feedbox"> 
+				<div class="nonSubCol"> Vote? <!-- Column names in feed -->
 				</div>
-				<div id="submission"> Feed				
+				<div id="subCol"> Feed				
 				</div>
-				<div id="checklist"> Answered/Read?
+				<div class="nonSubCol"> Answered/Read?
 				</div>	
-				<hr />						
+				
+				<hr />	
 			</div>
 			
 		</div>
 
 		
-		
-		<div class="bottomlinks">
-			<a class="aboutlink" href="about.php">About</a> | 
-			<a class="aboutlink" href="about.php">Privacy Policy</a> | 
-			<a class="aboutlink" href="about.php">Contact Us</a>
+		<div class="bottomlinks">	<!-- Links at bottom of page -->
+			<a class="aboutlink" href="help.php">About</a> | 
+			<a class="aboutlink" href="help.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="help.php">Contact Us</a>
 		</div>
 		
 	</body>
