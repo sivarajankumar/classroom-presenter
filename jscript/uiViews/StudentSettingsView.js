c1b44dd43a4c5a83d003e3eff49f88865519d12d
LiLKung0329 <LiLKung0329@localhost>
2011-04-29 21:48:16 -0700

    -added comments to bottom 7 views of uiViews

--- jscript/uiViews/StudentSettingsView.js
+++ jscript/uiViews/StudentSettingsView.js
@@ -1,1 +1,5 @@
-
+/*
+ * The StudentSettingsView renders the information retrieved from models,
+ * relating to user settings for students into a form suitable for interaction,
+ * a user interface element. StudentSettin.gsVewi inherits from SettingsView
+ */
