6367563900f48d27e2933a3871b1a8d781d266c9
schwenzr <schwenzr@localhost>
2011-04-29 21:52:07 -0700

    Updated the other half of the view classes with comments at the beginning to give a general description. All of the files should now have general comments about their purpose.

---
+++ jscript/beController/LoginController.js
@@ -0,0 +1,5 @@
+/* This controller handles when a user enters their 
+ * login credentials, looking them up in the database
+ * and verifying their identity. In addition, this
+ * controller will forward the user to the correct website
+ * based on whether they are a student or instructor. */
