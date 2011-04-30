99471667ea96921128b63166bd004b2b0b1295b7
schwenzr <schwenzr@localhost>
2011-04-29 21:52:07 -0700

    Updated the other half of the view classes with comments at the beginning to give a general description. All of the files should now have general comments about their purpose.

--- jscript/uiViews/FeedView.js
+++ jscript/uiViews/FeedView.js
@@ -2,5 +2,6 @@
  * This javascript file contains all of the state necessary
  * to update the view given to users of the feed. In addition, 
  * like other views, this file will contain fields that keep
- * track of the state of this view. 
+ * track of the state of this view. Note that this view is 
+ * intended towards students and not instructors.
  */
