398c04f3835583770bca1361ddc6ffe354128a2c
schwenzr <schwenzr@localhost>
2011-04-29 21:12:36 -0700

    Added comments at the beginning of each of the controller files. Also added comments at the beginning of each of the database script files.

--- jscript/beController/FeedController.js
+++ jscript/beController/FeedController.js
@@ -1,1 +1,4 @@
-
+/* This file deals with handling all of the interaction between
+ * the database and the feed. In addition, this file will have the 
+ * ability to update the feed based on the information contained in 
+ * the database. */
