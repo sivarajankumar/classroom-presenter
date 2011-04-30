13f152effeb45fbc7eee65e1b8d5dc865f1a971c
schwenzr <schwenzr@localhost>
2011-04-29 21:12:36 -0700

    Added comments at the beginning of each of the controller files. Also added comments at the beginning of each of the database script files.

--- jscript/beController/HistoryController.js
+++ jscript/beController/HistoryController.js
@@ -1,1 +1,5 @@
-//this is a stretch feature
+/* This controller handles all of the interaction
+ * with the database to deal with handling information
+ * from class sessions. In addition, it will be able to
+ * update information from the sessions if say for example
+ * an instructor answers a question after class. */ 
--- jscript/beController/SurveyController.js
+++ jscript/beController/SurveyController.js
@@ -1,1 +1,6 @@
-
+/* This controller interacts with the survey interface. The key goal
+ * of this javascript file is to act as a medium between the database
+ * and the survey data we are getting from the user. In addition,
+ * this controller will to interact between both students and 
+ * instructors in order to notify students of surveys and get
+ * the results back to instructors. */
