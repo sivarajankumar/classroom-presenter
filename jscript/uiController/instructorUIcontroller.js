7bc8d2dc163ccd0f46df58472383660f902209d3
schwenzr <schwenzr@localhost>
2011-04-29 21:33:10 -0700

    Made updates to all of the uiControllers and the FeedView with initial comments.

--- jscript/uiController/instructorUIcontroller.js
+++ jscript/uiController/instructorUIcontroller.js
@@ -1,4 +1,10 @@
-// function that allows the html to include multiple js files
+/*
+ * This javascript file hands all of the general events that occur
+ * on the instructor side. All events like clicks and updates will
+ * be handled by this controller. In addition, this file keeps track
+ * of some of the state associated with the instructor. 
+ */
+
 var currentSession;
 var timestamp;
 
