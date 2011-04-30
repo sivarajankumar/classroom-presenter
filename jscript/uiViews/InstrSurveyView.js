49b67fa80069738a557c2d64da3bd640fc2b54d4
LiLKung0329 <LiLKung0329@localhost>
2011-04-29 22:23:56 -0700

    -changed the path of the window location in the login.php

---
+++ jscript/uiViews/InstrSurveyView.js
@@ -0,0 +1,5 @@
+/*
+ * The InstrSurveyView renders the information retrieved from models,
+ * relating to instructor surveys into a form suitable for interaction,
+ * a user interface element. InstrSurveyView inherits SurveyView.
+
