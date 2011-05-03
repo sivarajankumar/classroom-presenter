a6681e0d698b009f9bd683aa3c061854d4fb4e5a
LiLKung0329 <LiLKung0329@localhost>
2011-05-03 15:35:32 -0700

    -fixed comment in instructor survey view

--- jscript/uiViews/InstrSurveyView.js
+++ jscript/uiViews/InstrSurveyView.js
@@ -2,4 +2,4 @@
  * The InstrSurveyView renders the information retrieved from models,
  * relating to instructor surveys into a form suitable for interaction,
  * a user interface element. InstrSurveyView inherits SurveyView.
-
+ */
