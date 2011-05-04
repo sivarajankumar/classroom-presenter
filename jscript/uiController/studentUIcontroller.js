794c3166b45c1052b9fc89d6f581f46483c24545
schwenzr <schwenzr@gmail.com>
2011-05-03 17:28:29 -0700

    Added a file that looks up questions and encodes the db response in json

---
+++ jscript/uiController/studentUIcontroller.js
@@ -0,0 +1,22 @@
+/*
+ * This file is similar to the instructor UI controller except that
+ * it handles events on the student side. So events like mouse clicks
+ * or updates will be sent through this function. In addition, like the 
+ * instructor UI controller, this controller will handle the all of the 
+ * state during the student's visit to Incognito. 
+ */
+
+var currentSession;
+var timestamp;
+
+function getContent(){
+
+}
+
+function onUpdate(){
+
+}
+
+function onSubmit(){
+
+}
---
+++ jscript/uiViews/HomeView.js
@@ -0,0 +1,6 @@
+/*
+ * This file contains all of the functions necessary to update the home
+ * portion of the website. In addition, this javascript file contains 
+ * all of the fields necessary to maintain the state of the home part 
+ * of Incognito.  
+ */
