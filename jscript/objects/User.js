e87f5ab6fb526657b002098a2fad0378e5d64ad3
shenama smd <shenama.smd@gmail.com>
2011-05-05 16:49:05 -0700

    added survery lookup page

---
+++ jscript/objects/User.js
@@ -0,0 +1,11 @@
+/*
+ * This module manages the behavior and attributes of User object,
+ * which is a User object, responds to requests for information about its state.
+ */
+
+var uid;
+var email;
+
+function validatePassword(password){
+
+}
