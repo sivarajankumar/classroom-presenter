7e12be558c7fb622d45f8b9d7ace846746f15846
shenama smd <shenama.smd@gmail.com>
2011-05-05 15:02:05 -0700

    inlcuded yui library for autocomplete

--- Incognito/students/studenthome.php
+++ Incognito/students/studenthome.php
@@ -8,6 +8,8 @@
 		
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="pages.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
+
 	</head>
 
 	<body>
