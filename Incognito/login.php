68dec6fdef6ffa3f6a93cd34e6df3da71d40db2b
Mike Kung <lilkung0329@gmail.com>
2011-05-05 17:09:10 -0700

    added submitting surveys in instructor side feature

---
+++ Incognito/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Incognito</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+		<div id="topbanner"> 			
+			<img src="logo.png" alt="logo" />
+		</div>			
+
+		
+		<div id="navigation">
+			<span class="tab">Welcome! Please log in below.	</span>
+		</div>
+		
+		<div id = "maincontent">			
+			<form method="post">
+				<div class="logincontent">
+					<div>
+						<div class="logintext">Sign me in as a...
+						</div>
+						<form>
+						<input type="button" class="loginType" value="Student" onclick="studenthome()">					
+						<input type="button" class="loginType" value="Instructor" onclick="instructorfeed()">
+						</form>
+						<script>
+							function studenthome(){
+								window.location = "students/studenthome.php";
+							}
+
+							function instructorfeed(){
+								window.location = "instr/instructorfeed.php"
+							}
+						</script>						
+						
+					</div>
+				</div>
+			</form>
+
+		</div> 
+
+		
+		
+		<div class="bottomlinks">	<!-- Links at bottom of page -->
+			<a class="aboutlink" href="">Report Bug</a> | 
+			<a class="aboutlink" href="help.php">About</a> | 
+			<a class="aboutlink" href="help.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="help.php">Contact Us</a>
+		</div>
+		
+	</body>
+</html>
---
+++ Incognito/students/studenthome.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Incognito</title>
+		
+		<script src="instructorfeed.js"></script>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
+
+	</head>
+
+	<body>
+		
+		<div id="topbanner"> 	<!-- Includes logo & person's information/help/logout, & course name -->
+			<img src="logo.png" alt="logo" />
+			<div id="greeting">
+				Hello [student's alias]! | <a href="studentsettings.php">Your Settings</a> |  <a class="aboutlink" href="help.php">Help</a> | <a href="login.php">Logout</a> <br />
+				You are currently looking at [course name].
+			</div>
+		</div>			
+
+		
+		<div id="navigation">	<!-- Navigation bar -->
+			<ul>
+				<li><span><a class="tab" href="studenthome.php">Home</a></span></li>
+				<li><span><a class="tab" href="studentfeed.php">Feed</a></span></li>
+				<li><span><a class="tab" href="studentsurveys.php">Surveys</a></span></li>
+			</ul>		
+		</div>
+		
+		<div id = "maincontent">
+			<form action="../../jscript/uiController/studentUIcontroller.js" method="post">
+				<div class="submissioncontent">	<!-- Includes: "Submit as", textbox, & submit button -->
+					<div id="typeAreaHome">
+						<span>Submit as:
+							<label><input type="radio" name="submitType" value="Q" checked="checked"/> Question</label>
+							<label><input type="radio" name="submitType" value="F"/> Feedback </label>
+						</span>
+						<textarea name="texthome" id="ac-input" rows="10" cols="80"></textarea>
+					</div>
+					<div id="submitbuttondiv"><button type="submit" id="submitbutton">Submit</button></div>
+				</div>
+			</form>
+		</div>
+<script>
+YUI({ filter: 'raw' }).use("autocomplete", "autocomplete-filters", "autocomplete-highlighters", function (Y) {
+  var states = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Dakota',
+    'North Carolina',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming'
+  ];
+
+  Y.one('#ac-input').plug(Y.Plugin.AutoComplete, {
+    resultFilters    : 'phraseMatch',
+    resultHighlighter: 'phraseMatch',
+    source           : states
+  });
+});
+</script>
+
+		
+		
+		<div class="bottomlinks">	<!-- Links at bottom of page -->
+			<a class="aboutlink" href="">Report Bug</a> | 
+			<a class="aboutlink" href="help.php">About</a> | 
+			<a class="aboutlink" href="help.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="help.php">Contact Us</a>
+		</div>
+		
+	</body>
+</html>
