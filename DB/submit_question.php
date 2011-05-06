cd2b4a6fb954db4e0c9e990b5ec6cc40b488b719
furbyownz <furbyownz@localhost>
2011-05-05 17:02:53 -0700

    Added function for getting questions for autocomplete to HomeView.js

---
+++ DB/submit_question.php
@@ -0,0 +1,30 @@
+<?php
+	
+	// Change this for whoever is using the php script
+	// These variables need to be changed for every person who sets this up
+	// Production DB: ashen; 2kV2cNct; ashen_403_Local
+	$username = "ashen";
+	$password = "2kV2cNct"; 
+	$db_name = "ashen_403_Local"; 
+
+	// Connect to server
+	$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+ 
+	if (!$db_conn) {
+		die("Failed to connect to the mysql server"); 
+	}
+	
+	// Select the correct database
+	mysql_select_db($database, $db_conn); 
+
+	// Insert the question into the table given
+	// all of the relevent fields
+	$answered = $_POST['answered'];
+	$numvotes = $_POST['numvotes'];
+	$text = $_POST['text'];
+	$sid = $_POST['sid']; 
+	$query = "INSERT INTO Question (qid, text, numvotes, answered, sid) VALUES ()";
+	mysql_query($query, $db_conn);
+	
+	
+?>
