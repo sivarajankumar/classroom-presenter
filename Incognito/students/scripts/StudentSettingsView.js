f10da1d875d6300573dc9efc8c427ce953cf92bb
schwenzr <schwenzr@gmail.com>
2011-05-11 23:55:34 -0700

    Created the first initial version of the getCourses function. It takes a student id and a handler function to handle the results returned back by the HTTP request.

---
+++ Incognito/students/scripts/StudentSettingsView.js
@@ -0,0 +1,19 @@
+/*
+ * The StudentSettingsView renders the information retrieved from models,
+ * relating to user settings for students into a form suitable for interaction,
+ * a user interface element. StudentSettin.gsVewi inherits from SettingsView
+ */
+
+// This function will makes an HTTP Request to the get_courses php script, retrieves
+// the courses, and gives the courses to the handleCourses handler. In addition, the
+// caller must pass a valid studentId. 
+function getCourses(studentId, handleCourses) {
+	
+	// Make the AJAX call to the backend
+	$.post("scripts/get_courses.php",
+			{uid: studentId},
+			function(data) {
+				results = JSON.parse(data);
+				handleCourses(results);
+			});
+}
