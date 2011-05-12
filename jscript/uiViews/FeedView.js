bb96ba1720fc8af50ce4afdde3e6be0e68a6e653
chriacua <chriacua@gmail.com>
2011-05-12 03:52:52 -0700

    for student settings and instructor settings: add/create&delete(its the same) course...bug when you add blank course-- cannot delete it

---
+++ jscript/uiViews/FeedView.js
@@ -0,0 +1,7 @@
+/*
+ * This javascript file contains all of the state necessary
+ * to update the view given to users of the feed. In addition, 
+ * like other views, this file will contain fields that keep
+ * track of the state of this view. Note that this view is 
+ * intended towards students and not instructors.
+ */
---
+++ jscript/uiViews/HelpView.js
@@ -0,0 +1,5 @@
+/*
+ * This javascript file contains all of the functions that update the 
+ * help feature of our appliceiation. In addition, this file contains 
+ * all of the fields that manage the state of the help view. 
+*/
