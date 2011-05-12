d08d7229a91c7ed20ce4189b0f6a2a1cbbc8b8f6
furbyownz <furbyownz@localhost>
2011-05-11 17:20:34 -0700

    Implemented the onSubmit() function in studentUIcontroller.js, which is triggered when the student home page's Submit button is clicked.

--- jscript/uiController/studentUIcontroller.js
+++ jscript/uiController/studentUIcontroller.js
@@ -4,9 +4,10 @@
  * or updates will be sent through this function. In addition, like the 
  * instructor UI controller, this controller will handle the all of the 
  * state during the student's visit to Incognito. 
- */
+ 
 $question = $_GET['texthome'];
 echo $question;
+*/
 
 var currentSession;
 var timestamp;
@@ -20,5 +21,38 @@ function onUpdate(){
 }
 
 function onSubmit(){
-
+	var text = submitform.elements["texthome"].value; // Haven't figured out the jQuery for this yet.
+	var type = $('input[name=submitType]:checked', '#submitform').val();
+	//alert( type );
+	var sid = 23456;
+	var dataString;
+	var numvotes;
+	if (type == 'Q')
+	{
+		var answered = 0;
+		numvotes = 1;
+		dataString = 'text=' + text + '&sid=' + sid + '&answered=' + answered + '&numvotes=' + numvotes + '&type=' + type;
+		$.ajax({
+			type: "POST",
+			url: "submit_question.php",
+			data: dataString,
+			success: function(){
+				//alert( 'Success!' );
+			}
+		});
+	}
+	else if (type == 'F')
+	{
+		numvotes = 1;
+		var isread = 0;
+		dataString = 'text=' + text + '&sid=' + sid + '&numvotes=' + numvotes + '&isread=' + isread + '&type=' + type;
+		$.ajax({
+			type: "POST",
+			url: "submit_question.php",
+			data: dataString,
+			success: function(){
+				//alert( 'Success!' );
+			}
+		});
+	}
 }
