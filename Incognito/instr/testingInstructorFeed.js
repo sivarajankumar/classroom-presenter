ea07c258a36dd6c22ade8f873bf97210026dccd6
chriacua <chriacua@gmail.com>
2011-05-12 09:20:28 -0700

    instructorfeed is populated with questions (minus the checkboxes) and can sort by newest or highest priority

---
+++ Incognito/instr/testingInstructorFeed.js
@@ -0,0 +1,28 @@
+ $(document).ready(function() { 
+	$('<p class=\"removeAll\">3</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 1</p>').appendTo('#feedCol');
+	$('<p class=\"removeAll\">1</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 2</p>').appendTo('#feedCol');
+	$('<p class=\"removeAll\">4</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 3</p>').appendTo('#feedCol');
+	
+   $("#priority").click(function() {
+	$(".removeAll").detach();
+	$('<p class=\"removeAll\">4</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 3</p>').appendTo('#feedCol');
+	$('<p class=\"removeAll\">3</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 1</p>').appendTo('#feedCol');
+	$('<p class=\"removeAll\">1</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 2</p>').appendTo('#feedCol');
+	});
+	
+	$("#newest").click(function() {
+	$(".removeAll").detach();
+	$('<p class=\"removeAll\">3</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 1</p>').appendTo('#feedCol');
+	$('<p class=\"removeAll\">1</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 2</p>').appendTo('#feedCol');
+	$('<p class=\"removeAll\">4</p>').appendTo('#viewVotesCol');
+	$('<p class=\"removeAll\">Question 3</p>').appendTo('#feedCol');
+	});
+});
