31f1489a080bac55be3fe8009a775e4fbb5195f7
schwenzr <schwenzr@gmail.com>
2011-05-12 22:22:56 -0700

    There was a couple of errors with the HTML strings that were being returned

---
+++ Incognito/instr/scripts/addStudent.php
@@ -0,0 +1,45 @@
+<?php
+
+	// This php script adds a student to a class given a email and a 
+	// course id
+	
+	// Check if the email and cid have been set
+	if (isset($_POST['email']) && isset($_POST['cid'])) {
+		
+		// Connect to the database
+		$username = "schwer";
+		$password = "Egh8vF5d";
+		$db_name = "schwer_Incognito";
+
+		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+		if (!$db_conn) {
+			die("Could not connect");
+		}
+
+		mysql_select_db($db_name, $db_conn);
+		
+		// First get the uid for the student based off of the email
+		$email = $_POST['email'];
+		$cid = $_POST['cid'];
+		$query = sprintf("SELECT uid FROM User WHERE email = '%s';", $email);
+		$results = mysql_query($query, $db_conn);
+		
+		// Check for errors
+		if (!$results) {
+			die("Error: " + mysql_error());
+		}
+		
+		$row = mysql_fetch_row($results);
+
+		// Now insert the student, session pair in the attends table
+		$uid = $row[0];
+		$query = sprintf("INSERT INTO Attends (uid, cid) VALUES (%d, %d);", $uid, $cid);
+		$results = mysql_query($query, $db_conn);
+
+		// Do some error checking
+		if (!$results) {
+			die("Error: " + mysql_error($db_conn));
+		}
+	}
+
+?>
---
+++ Incognito/students/scripts/delete_course.php
@@ -0,0 +1,33 @@
+<?php
+
+	// This php file will delete a course from a student's course list
+	// given a uid for the student and a cid for the course
+	
+	// Check if we are given a uid and a cid
+	if (isset($_POST['uid']) && isset($_POST['cid'])) {
+		
+		// Connect to the database
+		$username = "schwer";
+		$password = "Egh8vF5d";
+		$db_name = "schwer_Incognito";
+		
+		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+		if (!$db_conn) {
+			die("Could not connect");
+		}
+		
+		mysql_select_db($db_name, $db_conn);
+		
+		// Now run the query on the database
+		$uid = $_POST['uid'];
+		$cid = $_POST['cid'];
+		$query = sprintf("DELETE FROM Attends WHERE uid = %d AND cid = %d;", $uid, $cid);
+		$results = mysql_query($query, $db_conn);
+		
+		// Check for errors
+		if (!$results) {
+			die("Error: " + mysql_error($db_conn));
+		}
+	}
+
+?>
