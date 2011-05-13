4dc9021d7c23640a093eb9e0f8878de2757cbc2e
schwenzr <schwenzr@gmail.com>
2011-05-12 20:00:04 -0700

    Changed the POST's to GET's in my php script and forgot to change them back. My bad. The updateStudent function works however.

--- Incognito/instr/scripts/addStudent.php
+++ Incognito/instr/scripts/addStudent.php
@@ -4,7 +4,7 @@
 	// course id
 	
 	// Check if the email and cid have been set
-	if (isset($_GET['email']) && isset($_GET['cid'])) {
+	if (isset($_POST['email']) && isset($_POST['cid'])) {
 		
 		// Connect to the database
 		$username = "schwer";
@@ -19,8 +19,8 @@
 		mysql_select_db($db_name, $db_conn);
 		
 		// First get the uid for the student based off of the email
-		$email = $_GET['email'];
-		$cid = $_GET['cid'];
+		$email = $_POST['email'];
+		$cid = $_POST['cid'];
 		$query = sprintf("SELECT uid FROM User WHERE email = '%s';", $email);
 		$results = mysql_query($query, $db_conn);
 		
