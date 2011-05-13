730ff25fe52529bb50d90887124aab61a49b87c7
shenama smd <shenama.smd@gmail.com>
2011-05-13 15:13:38 -0700

    added db credentials

---
+++ Incognito/students/scripts/db_credentials.php
@@ -0,0 +1,16 @@
+<?php
+		// Connect to the production database
+		$username = "ashen";
+		$password = "2kV2cNct";
+		$db_name = "ashen_403_Local";
+		
+		// Connect to your own db
+		
+		//$username = "schwer";
+		//$password = "Egh8vF5d";
+		//$db_name = "schwer_Incognito";
+		
+		//$username = "chriacua";
+		//$password = "b67wwpAH";
+		//$db_name = "chriacua_testui";
+?>
