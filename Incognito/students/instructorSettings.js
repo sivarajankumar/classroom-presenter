08a00580c1107404abaf096f20c687c59696f1dc
furbyownz <furbyownz@localhost>
2011-05-13 19:21:04 -0700

    removed a debugging echo from lookup_questions

---
+++ Incognito/students/instructorSettings.js
@@ -0,0 +1,54 @@
+ /* This file handles the events for Instructor Settings page. */
+  function printToScreen(data){
+	$("#courseInfo").html(data);
+ }
+ 
+ $(document).ready(function() {
+	var sessionId;
+	var courseId;
+	var mailIs;
+	//initially puts courses on page
+
+	getCourses($.cookie("session"),printToScreen;
+ 
+	// starts a session
+   $(".closeOptionButton").live('click',function(event) {
+		sessionId = startSession($(this).attr('id'));
+	});
+   
+    //ends a session
+	$(".openOptionButton").live('click',function(event) { 
+		endSession(sessionId);
+	});
+	
+	//Adds a InstructorID, courseName, mailingList, callbackFunction
+	$("#courseSubmitButton").click(function(event){
+		var courseIs = $("#courseName").val();
+		mailIs = $("#mailingList").val();
+		courseId = createCourse($.cookie("session"), courseIs, mailIs, printToScreen);
+		getCourses($.cookie("session"),printToScreen);
+	});
+	
+	//user deletes a course
+	$(".courseRemoveButton").live('click',function(event) {
+		deleteCourse($.cookie("session"), $(this).attr('id'), mailIs, printToScreen);
+		getCourses($.cookie("session"),printToScreen);
+	});
+	
+	
+	//Adds a student
+	$("#addStudentButton").click(function(event){
+		var student = $("#studentToAdd").val();
+		var course = $("#studentCourseButton").val();
+		updateStudents(student, course);
+		alert("student added!");
+	});
+});
+ 
+ 
+ 
+ 
+ 
+
+
+
