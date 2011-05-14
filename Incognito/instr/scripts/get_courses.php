8e96dcee6cfa95cdedc3780a17d8f688257c4a6b
schwenzr <schwenzr@gmail.com>
2011-05-13 17:38:47 -0700

    Email was getting passed to the AJAX call, not the uid, so I had to change some stuff in the scripts and the AJAX call.

---
+++ Incognito/instr/scripts/get_courses.php
@@ -0,0 +1,45 @@
+<?php
+	include 'db_credentials.php';
+	// This php script get's all of the courses that a instructor 
+	// teaches and prints some html for buttons
+	
+	// Check if we are given an email
+	if (isset($_POST['mail'])) {
+		
+		// Connect to our database (change for different user) 
+		// Connect to the database
+		//$username = "ashen";
+		//$password = "2kV2cNct";
+		//$db_name = "ashen_403_Local";
+		
+		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+		if (!$db_conn) {
+			die("Could not connect");
+		}
+		
+		mysql_select_db($db_name, $db_conn);
+		
+		// Query the database for all courses that the teacher teaches
+		$email = $_POST['mail'];
+		$query = sprintf("SELECT c.cid, c.name, s.open FROM User u, Teaches t, Course c, Session s WHERE u.email = '%s' AND t.uid = u.uid AND t.cid = c.cid AND s.cid = t.cid;", $email);
+		$results = mysql_query($query, $db_conn);
+		
+		// Then return the results in HTML form
+		while ($row = mysql_fetch_row($results)) {
+			
+			// Check if the course is closed
+			if ($row[2] == 1) {
+				
+				echo  "<br />$row[1]:<button class='closeOptionButton'  id='$row[0]'>Close?</button>
+						<button id='$row[0]' class='courseRemoveButton'>Delete</button>";
+		
+			} else {
+				
+				echo  "<br />$row[1]:<button class='openOptionButton' id='$row[0]'>Open?</button>
+						<button id='$row[0]' class='courseRemoveButton'>Delete</button>";
+				
+			}
+		}
+	}
+
+?>
