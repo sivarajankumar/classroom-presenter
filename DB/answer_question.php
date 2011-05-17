ee50a283396b0a86ef9ba441573b641ae19ce693
furbyownz <furbyownz@localhost>
2011-05-16 18:49:56 -0700

    answer_question should be working now

--- DB/answer_question.php
+++ DB/answer_question.php
@@ -1,26 +1,23 @@
 <?php
 
-	// Connect to the database
-
-	// These variables need to be changed for every person who wants to use their local db.
-	// Production DB: ashen; 2kV2cNct; ashen_403_Local
 	$username = "ashen";
-	$password = "2kV2cNct"; 
-	$db_name = "ashen_403_Local"; 
+	$password = "2kV2cNct";
+	$db_name = "ashen_403_Local";
 
 	$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
- 
+
 	if (!$db_conn) {
-		die("Failed to connect to the mysql server"); 
+		die("Failed to connect: " . mysql_error());
 	}
 
-	mysql_select_db($db_name, $db_conn); 
-	
-	$type = $_POST['type'];
-	$id = $_POST['id'];
-	$flag = $_POST['answer'];
+	mysql_select_db($db_name, $db_conn);
+
+	$type = $_POST['type'];	// 'Q' for question, 'F' for feedback
+	$id = $_POST['id'];		// ID for the question or feedback
+	$flag = $_POST['flag'];	// "true" if we want to set the entry to answered/read, "false" for unanswered/not read
+
 	$query = null;
-	
+
 	if ($type == 'Q')
 	{
 		if ($flag == "true")
@@ -29,11 +26,9 @@
 		}
 		else
 		{
-			$query = sprintf("UPDATE Question SET answered = 0 WHERE qid = %d", %id)
+			$query = sprintf("UPDATE Question SET answered = 0 WHERE qid = %d", $id);
 		}
-		
-		if(!mysql_query($query, $db_conn))
-		{
+		if (!mysql_query($query, $db_conn)) {
 			die("Query error: " . mysql_error());
 		}
 	}
@@ -41,17 +36,16 @@
 	{
 		if ($flag == "true")
 		{
-			$query = sprintf("UPDATE Feedback SET isread = 1 WHERE fid = %d", $id);
+			$query = sprintf("UPDATE Feedback SET isread = 1 WHERE qid = %d", $id);
 		}
 		else
 		{
-			$query = sprintf("UPDATE Feedback SET isread = 0 WHERE fid = %d", $id);
+			$query = sprintf("UPDATE Feedback SET isread = 0 WHERE qid = %d", $id);
 		}
-		
-		if(!mysql_query($query, $db_conn))
-		{
+		if (!mysql_query($query, $db_conn)) {
 			die("Query error: " . mysql_error());
 		}
 	}
-	
+	mysql_close($db_conn);
+
 ?>
