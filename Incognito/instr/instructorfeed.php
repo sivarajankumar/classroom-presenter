7512a2db78945975b33c90694438404fc3e13c7d
chriacua <chriacua@gmail.com>
2011-05-19 09:08:34 -0700

    popup window works on instructor feed but timeline not working quite yet

--- Incognito/instr/instructorfeed.php
+++ Incognito/instr/instructorfeed.php
@@ -11,6 +11,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="pages.css" type="text/css" rel="stylesheet" />
 		<script src="jquery-1.5.2.js" type="text/javascript"></script>
+		
 		<script type="text/javascript" src="jquery.cookie.js"></script>
         <script type="text/javascript" src="instructorfeed.js"></script>
         <script type="text/javascript" src="scripts/InstrSettingsView.js"></script>
