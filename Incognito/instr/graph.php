7fca8763185c62b238401040438d180510024585
chriacua <chriacua@gmail.com>
2011-05-19 14:33:03 -0700

    graph appears and populates; not a line graph like original plan; has 2 test buttons (start & stop) that will most likely change in integration

--- Incognito/instr/graph.php
+++ Incognito/instr/graph.php
@@ -4,15 +4,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<!-- CSE 190 M Homework 4 (NerdLuv) -->
 	<head>
-		<title>change</title>
-		<script src="jquery.js"></script>
+		<title>adf</title>
+		<script src="jquery-1.5.2.js"></script>
 		<script src="plotWindow.js"></script>
-		<script src="jquery.flot.js"></script>
-		<link rel="stylesheet" type="text/css" href="pages.css" />
+		<link rel="stylesheet" type="text/css" href="pages.css" />		
+		<script language="javascript" type="text/javascript" src="jquery.flot.js"></script>
 	</head>
 
 	<body>
-		<button id="test1">test1</button><button id="test2">test2</button>
+		<button id="test1">test button</button><button id="test2">test button</button>
 		<div id="graphPage">
 			
 		</div>
