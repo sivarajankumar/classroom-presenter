79af90d4cc8f3b547814226ac43b2fa5e865506f
schwenzr <schwenzr@gmail.com>
2011-05-19 23:27:19 -0700

    Forgot to add the db_credentials.php file at the beginning

--- Incognito/instr/scripts/timeline.php
+++ Incognito/instr/scripts/timeline.php
@@ -4,9 +4,11 @@
 	// the number of questions asked in the last 5 minutes (this may change)
 	// In addition, we are given a time so that we can return an array that
 	// has the following format: a = {time, count}
+
+	include 'db_credentials.php'; 
 	
 	// Ensure that we are given a session id
-	if (isset($_POST['id']) && isset($_POST['time'])) {
+	if (isset($_GET['id']) && isset($_GET['time'])) {
 		
 		// Connect and select the correct database
 		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
@@ -18,7 +20,7 @@
 		
 		// Query the database for all of the questions in this 
 		// session that are within 5 minutes of the time
-		$sid = $_POST['sid'];
+		$sid = $_GET['sid'];
 		$query = sprintf("SELECT count(*) FROM Question WHERE ((NOW() - time) < 5)
 							AND sid = %d;", $sid);
 		$results = mysql_query($query, $db_conn);
@@ -30,7 +32,7 @@
 		
 		// Do the response
 		$row = mysql_fetch_row($results);
-		$time = $_POST['time'];
+		$time = $_GET['time'];
 		$response[0] = $time; 
 		$response[1] = $row[0];
 		
