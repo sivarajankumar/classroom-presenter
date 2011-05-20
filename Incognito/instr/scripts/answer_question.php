5adf9e4591c9f5b8606d5f062837e37fc1be31ea
furbyownz <furbyownz@localhost>
2011-05-20 01:42:33 -0700

    Reorganized feed-related PHP scripts (now they have functions)

--- Incognito/instr/scripts/answer_question.php
+++ Incognito/instr/scripts/answer_question.php
@@ -1,51 +1,64 @@
 <?php
 
-	$username = "ashen";
-	$password = "2kV2cNct";
-	$db_name = "ashen_403_Local";
+	function connectToDB()
+	{
+		// Connect to the database
 
-	$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+		// These variables need to be changed for every person who wants to use their local db.
+		// Production DB: ashen; 2kV2cNct; ashen_403_Local
+		$username = "ashen";
+		$password = "2kV2cNct"; 
+		$db_name = "ashen_403_Local"; 
 
-	if (!$db_conn) {
-		die("Failed to connect: " . mysql_error());
+		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+	 
+		if (!$db_conn) {
+			die("Failed to connect to the mysql server"); 
+		}
+
+		mysql_select_db($db_name, $db_conn);
+		return $db_conn;
 	}
 
-	mysql_select_db($db_name, $db_conn);
+	function answer($type, $id, $flag)
+	{
+		$db_conn = connectToDB();
 
+		$query = null;
+
+		if ($type == 'Q')
+		{
+			if ($flag == "true")
+			{
+				$query = sprintf("UPDATE Question SET answered = 1 WHERE qid = %d", $id);
+			}
+			else
+			{
+				$query = sprintf("UPDATE Question SET answered = 0 WHERE qid = %d", $id);
+			}
+			if (!mysql_query($query, $db_conn)) {
+				die("Query error: " . mysql_error());
+			}
+		}
+		elseif ($type == 'F')
+		{
+			if ($flag == "true")
+			{
+				$query = sprintf("UPDATE Feedback SET isread = 1 WHERE qid = %d", $id);
+			}
+			else
+			{
+				$query = sprintf("UPDATE Feedback SET isread = 0 WHERE qid = %d", $id);
+			}
+			if (!mysql_query($query, $db_conn)) {
+				die("Query error: " . mysql_error());
+			}
+		}
+		mysql_close($db_conn);
+	}
+	
 	$type = $_POST['type'];	// 'Q' for question, 'F' for feedback
 	$id = $_POST['id'];		// ID for the question or feedback
 	$flag = $_POST['flag'];	// "true" if we want to set the entry to answered/read, "false" for unanswered/not read
-
-	$query = null;
-
-	if ($type == 'Q')
-	{
-		if ($flag == "true")
-		{
-			$query = sprintf("UPDATE Question SET answered = 1 WHERE qid = %d", $id);
-		}
-		else
-		{
-			$query = sprintf("UPDATE Question SET answered = 0 WHERE qid = %d", $id);
-		}
-		if (!mysql_query($query, $db_conn)) {
-			die("Query error: " . mysql_error());
-		}
-	}
-	elseif ($type == 'F')
-	{
-		if ($flag == "true")
-		{
-			$query = sprintf("UPDATE Feedback SET isread = 1 WHERE qid = %d", $id);
-		}
-		else
-		{
-			$query = sprintf("UPDATE Feedback SET isread = 0 WHERE qid = %d", $id);
-		}
-		if (!mysql_query($query, $db_conn)) {
-			die("Query error: " . mysql_error());
-		}
-	}
-	mysql_close($db_conn);
-
+	answer($type, $id, $flag);
 ?>
--- Incognito/students/scripts/submit_vote.php
+++ Incognito/students/scripts/submit_vote.php
@@ -2,45 +2,49 @@
 
 	// This script submits a vote for a question or feedback
 
-	// Connect to the database
+	function connectToDB()
+	{
+		// Connect to the database
 
-	// These variables need to be changed for every person who wants to use their local db.
-	// Production DB: ashen; 2kV2cNct; ashen_403_Local
-	$username = "ashen";
-	$password = "2kV2cNct"; 
-	$db_name = "ashen_403_Local"; 
+		// These variables need to be changed for every person who wants to use their local db.
+		// Production DB: ashen; 2kV2cNct; ashen_403_Local
+		$username = "ashen";
+		$password = "2kV2cNct"; 
+		$db_name = "ashen_403_Local"; 
 
-	$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
- 
-	if (!$db_conn) {
-		die("Failed to connect to the mysql server"); 
+		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+	 
+		if (!$db_conn) {
+			die("Failed to connect to the mysql server"); 
+		}
+
+		mysql_select_db($db_name, $db_conn);
+		return $db_conn;
 	}
-
-	mysql_select_db($db_name, $db_conn); 
 	
-	$type = $_POST['type'];			// Type of the submission ('Q' or 'F')
-	$id = $_POST['id'];				// ID of the question or feedback
-	$username = $_POST['username'];	// User's alias
-	$vote = $_POST['vote'];			// "true" if we want to submit a vote, "false" if we want to undo a previous submission
-	
-	// Do a preliminary query to get the student's user ID for later use
-	$uidquery = sprintf("SELECT uid FROM Student WHERE alias = '%s'", $username);
-	$uidresult = mysql_query($uidquery, $db_conn);
-	$uidrow = mysql_fetch_assoc($uidresult);
-	$uid = (int)$uidrow["uid"];	
-
-	if ( $type == 'Q' )
+	function getCurrentVotes($type, $id, $db_conn)
 	{
 		// Get the current vote count
-		$query = sprintf("SELECT numvotes FROM Question WHERE qid = %d", $id);
+		if ($type == 'Q')
+		{
+			$query = sprintf("SELECT numvotes FROM Question WHERE qid = %d", $id);
+		}
+		else
+		{
+			$query = sprintf("SELECT numvotes FROM Feedback WHERE fid = %d", $id);
+		}
+		
 		$result = mysql_query($query, $db_conn);
 		$r = mysql_fetch_assoc($result);
-		$vote_count = (int)$r["numvotes"];		
-
-		if ($vote == "true")
+		$vote_count = (int)$r["numvotes"];	
+		return $vote_count;
+	}
+	
+	function submitVote($type, $vote_count, $id, $db_conn)
+	{
+		$vote_count = $vote_count + 1;
+		if ($type == 'Q')
 		{
-			// submit a vote
-			$vote_count = $vote_count + 1;
 			$query = sprintf("UPDATE Question SET numvotes = %d WHERE qid = %d", $vote_count, $id);
 			if(!mysql_query($query, $db_conn)) {
 				die("Query error: " . mysql_error());
@@ -54,33 +58,6 @@
 		}
 		else
 		{
-			// undo a vote submission
-			$vote_count = $vote_count - 1;
-			$query = sprintf("UPDATE Question SET numvotes = %d WHERE qid = %d", $vote_count, $id);
-			if(!mysql_query($query, $db_conn)) {
-				die("Query error: " . mysql_error());
-			}
-			else {
-				$query = sprintf("DELETE FROM QuestionVotedOn WHERE qid = %d AND uid = %d", $id, $uid);
-				if(!mysql_query($query, $db_conn)) {
-					die("Query error: " . mysql_error());
-				}
-			}
-		}
-		
-	}
-	elseif ( $type == 'F' )
-	{
-		// Get the current vote count
-		$query = sprintf("SELECT numvotes FROM Feedback WHERE fid = %d", $id);
-		$result = mysql_query($query, $db_conn);
-		$r = mysql_fetch_assoc($result);
-		$vote_count = (int)$r["numvotes"];
-		
-		if ($vote == "true")
-		{
-			// submit a vote
-			$vote_count = $vote_count + 1;
 			$query = sprintf("UPDATE Feedback SET numvotes = %d WHERE fid = %d", $vote_count, $id);
 			if(!mysql_query($query, $db_conn)) {
 				die("Query error: " . mysql_error());
@@ -92,10 +69,27 @@
 				}
 			}
 		}
-		else 
+	}
+	
+	function undoVote($type, $vote_count, $id, $db_conn)
+	{
+		$vote_count = $vote_count - 1;
+		
+		if ($type == 'Q')
 		{
-			// undo a vote submission
-			$vote_count = $vote_count - 1;
+			$query = sprintf("UPDATE Question SET numvotes = %d WHERE qid = %d", $vote_count, $id);
+			if(!mysql_query($query, $db_conn)) {
+				die("Query error: " . mysql_error());
+			}
+			else {
+				$query = sprintf("DELETE FROM QuestionVotedOn WHERE qid = %d AND uid = %d", $id, $uid);
+				if(!mysql_query($query, $db_conn)) {
+					die("Query error: " . mysql_error());
+				}
+			}
+		}
+		else
+		{
 			$query = sprintf("UPDATE Feedback SET numvotes = %d WHERE qid = %d", $vote_count, $id);
 			if(!mysql_query($query, $db_conn)) {
 				die("Query error: " . mysql_error());
@@ -107,7 +101,57 @@
 				}
 			}
 		}
+	}
+	
+	function processVote($type, $id, $username, $vote)
+	{
+		$db_conn = connectToDB();
 		
+		// Do a preliminary query to get the student's user ID for later use
+		$uidquery = sprintf("SELECT uid FROM Student WHERE alias = '%s'", $username);
+		$uidresult = mysql_query($uidquery, $db_conn);
+		$uidrow = mysql_fetch_assoc($uidresult);
+		$uid = (int)$uidrow["uid"];	
+
+		if ( $type == 'Q' )
+		{
+			// Get the current vote count
+			$vote_count = getCurrentVotes($type, $id, $db_conn);	
+
+			if ($vote == "true")
+			{
+				// submit a vote
+				submitVote($type, $vote_count, $id, $db_conn);
+			}
+			else
+			{
+				// undo a vote submission
+				undoVote($type, $vote_count, $id, $db_conn);
+			}
+			
+		}
+		elseif ( $type == 'F' )
+		{
+			// Get the current vote count
+			$vote_count = getCurrentVotes($type, $id, $db_conn);
+			
+			if ($vote == "true")
+			{
+				// submit a vote
+				submitVote($type, $vote_count, $id, $db_conn);
+			}
+			else 
+			{
+				// undo a vote submission
+				undoVote($type, $vote_count, $id, $db_conn);
+			}
+			
+		}
 	}
 
+	$type = $_POST['type'];			// Type of the submission ('Q' or 'F')
+	$id = $_POST['id'];				// ID of the question or feedback
+	$username = $_POST['username'];	// User's alias
+	$vote = $_POST['vote'];			// "true" if we want to submit a vote, "false" if we want to undo a previous submission
+	processVote($type, $id, $username, $vote);
 ?>
