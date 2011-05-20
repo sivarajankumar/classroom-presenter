60444a9e0f1ae79019f1dfacbd99596f6ff2ef20
Mike Kung <lilkung0329@gmail.com>
2011-05-20 13:47:30 -0700

    added styling for survey feed

---
+++ Incognito/help_instructors.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Incognito</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+		<div id="topbanner"> 			
+			<img src="logo.png" alt="logo" />
+		</div>			
+
+		
+		<div id="navigation">	<!-- Navigation bar -->
+			<ul>
+				<li><span><a class="tab" href="instructorfeed.php">Feed</a></span></li>
+				<li><span><a class="tab" href="instructorfree.php">Surveys</a></span></li>
+				<li><span><a class="tab" href="instructorhistory.php">History</a></span></li>
+			</ul>		
+			<a href=""><span id="timeline">VIEW TIMELINE </span></a>
+		</div>
+		
+		<div id = "maincontent">
+		<big><strong>Create a Course:</strong></big><br />
+		1. Go to the settings page<br />
+		2. In the create a course form, give your course a descriptive name(eg CSE440_spring) and enter a mailing list(optional). <br />
+		3. Click create. <br />
+		<br />
+		<big><strong>Add a Student to a Course:</strong></big><br />
+		1. Go to the settings page <br />
+		2. In the add students form, type in the course name (the name that is listed in the course list.) and the students CSE NETID <br />
+		3. Click add. <br />
+		<br />
+		<big><strong>Open a Class Feed:</strong></big><br />
+		1. Go to the settings page. <br />
+		2. Make sure all other course sessions are closed. <br />
+		3. Click the open session button next to the course feed you want to open. <br />
+		<br />
+		<big><strong>Close a Class Feed:</strong></big><br />
+		1. Go to the settings page. <br />
+		2. If the class session is open, you should see the close session button next to that course. <br />
+		3. Click the close session button. <br />
+		<br />
+		<big><strong>Using the Timeline Graph:</strong></big><br />
+		1. Open the current session’s feed. <br />
+		2. From the instructor feed or survey page, click the “View Timeline” button on the right side of the navigation bar. The line graph window will appear. <br />
+		3. To hide the line graph, minimize its window. <br />
+		<br />
+		<big><strong>Creating a Multiple Choice Poll:</strong></big><br />
+		1. On the instructor survey page, select “Multiple Choice” from the options above the text box. <br />
+		2. Type the poll’s prompt into the text box. <br />
+		3. Type the answer options into the fields below the text box. To add another option, click the “+” button to the right of the last option. To remove an option, click the “-” button to the right of the last option.<br /> 
+		4. Click “Create”.<br />
+		<br />
+		<big><strong>Creating a Free Response Question:</strong></big><br />
+		1. On the instructor survey page, select “Free Response” from the options above the text box.<br /> 
+		2. Type the question’s prompt into the text box.<br /> 
+		3. Click “Create”. <br />
+		<br />
+		<big><strong>Close a Survey:</strong></big><br />
+		1. Open the instructor survey page. <br />
+		2. Select “Closed” from the drop-down menu left of the survey you want to close. (Note: A survey can only be closed if its status is currently “Open.”<br />
+		<br />
+		<big><strong>View Survey Responses:</strong></big><br />
+		1. Open the instructor survey page. <br />
+		2. Once a survey has been closed, a drop arrow will appear under “View Responses” column. Select “Yes” from the drop-down menu to the right of the desired survey.<br />
+		<br /> 
+		<big><strong>Flag a Submission as Spam:</strong></big><br />
+		1. Open the instructor feed page. <br />
+		2. Click the flag button next to the response you want to flag. <br />
+		<br />		
+		</div>
+
+		
+		
+		<div class="bottomlinks">	<!-- Links at bottom of page -->
+			<a class="aboutlink" href="">Report Bug</a> | 
+			<a class="aboutlink" href="help.php">About</a> | 
+			<a class="aboutlink" href="help.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="help.php">Contact Us</a>
+		</div>
+		
+	</body>
+</html>
---
+++ Incognito/students/studentsurveys.php
@@ -0,0 +1,86 @@
+<?php
+    include "../get_uid.php";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Incognito</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+		<script src="jquery-1.5.2.js" type="text/javascript"></script>
+		<script type="text/javascript" src="jquery.cookie.js"></script>
+		<script type="text/javascript" src="scripts/StudentSettingsView.js"></script>
+        <script type="text/javascript" src="jquery-impromptu.3.1.js"></script>
+        <script type="text/javascript" src="scripts/StudentSurveyView.js"></script>
+        <script type="text/javascript" src="studentsurveys.js"></script>
+	</head>
+
+	<body>
+		
+		<div id="topbanner"> 
+			
+			<img src="logo.png" alt="logo" />
+			<div id="greeting">
+				<span id="cook"><?php echo 'Hello '.($_COOKIE['alias']!='' ? $_COOKIE['alias'] : 'Guest') ?></span>  | <a href="studentsettings.php">Your Settings</a> |  <a class="aboutlink" href="help.php">Help</a> | <a href="scripts/logout.php">Logout</a> <br />
+			</div>
+		</div>			
+
+		
+		<div id="navigation">
+			<ul>
+				<li><span><a class="tab" href="studenthome.php">Home</a></span></li>
+				<li><span><a class="tab" href="studentfeed.php">Feed</a></span></li>
+				<li><span><a class="tab" href="studentsurveys.php">Surveys</a></span></li>
+			</ul>		
+		</div>
+		
+		<div id = "maincontent">
+			<div id="filterandsort">
+				<span>
+					FILTER BY: 
+					<select name="filter" id="filter">
+                        <optgroup>
+                            <option selected="selected"> None </option>
+                            <option>Free Response</option>
+                            <option>Multiple Choice</option>
+                        </optgroup>
+					</select>
+				</span>
+				
+				<span>
+					SORT BY:
+					<a id="newest" >NEWEST</a> | <a id="priority" >HIGHEST PRIORITY</a>
+				</span>
+
+			</div>
+			
+			<div id="feedbox">
+				<span>
+					<div class="nonSubCol"> Survey Type
+					</div>
+					<div id="subCol"> Question				
+					</div>
+					<div class="nonSubCol"> Respond?
+					</div>	
+					<hr />		
+				</span>				
+			</div>
+
+			<div id="feed"></div>
+            
+		</div>
+
+		
+		
+		<div class="bottomlinks">
+			<a class="aboutlink" href="">Report Bug</a> | 
+			<a class="aboutlink" href="about.php">About</a> | 
+			<a class="aboutlink" href="about.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="about.php">Contact Us</a>
+		</div>
+		
+	</body>
+</html>
