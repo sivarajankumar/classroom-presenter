a5ad7cdb4a0d8e9e21a2f7f3f0d3056b97812e29
shenama smd <shenama.smd@gmail.com>
2011-05-20 00:40:26 -0700

    small step for mankind here: logout

---
+++ Incognito/students/scripts/delete_course.php
@@ -0,0 +1,46 @@
+<?php
+	include 'db_credentials.php';
+	// This php file will delete a course from a student's course list
+	// given a uid for the student and a cid for the course
+	
+	// Check if we are given a uid and a cid
+	if (isset($_POST['email']) && isset($_POST['cid'])) {
+		
+		// Connect to the database
+		//$username = "schwer";
+		//$password = "Egh8vF5d";
+		//$db_name = "schwer_Incognito";
+	
+		
+		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+		if (!$db_conn) {
+			die("Could not connect");
+		}
+		
+		mysql_select_db($db_name, $db_conn);
+		
+		// Now run the query on the database
+		$email = $_POST['email'];
+		$query = sprintf("SELECT uid FROM User WHERE email = '%s';", $email); 
+		$results = mysql_query($query, $db_conn); 
+		
+		// Check for errors
+		if (!$results) {
+			die("Error: " + mysql_error($db_conn)); 
+		}
+
+		$row = mysql_fetch_row($results); 
+		$uid = $row[0];
+
+		$cid = $_POST['cid'];
+		$query = sprintf("DELETE FROM Attends WHERE uid = %d AND cid = %d;", $uid, $cid);
+		$results = mysql_query($query, $db_conn);
+		
+		echo $query; 	
+		// Check for errors
+		if (!$results) {
+			die("Error: " + mysql_error($db_conn));
+		}
+	}
+
+?>
