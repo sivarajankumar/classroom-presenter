21c26411ed6e13842a861b1f50020c1f9998fc33
schwenzr <schwenzr@gmail.com>
2011-05-20 01:42:18 -0700

    Added some additional comments about the arguments for createMC.

--- Incognito/instr/scripts/InstrSurveyView.js
+++ Incognito/instr/scripts/InstrSurveyView.js
@@ -3,7 +3,8 @@ for instructors to interact with the state in the
 database. */
 
 // This function takes a session id and question text, 
-// then creates a new multiple choice survey. 
+// then creates a new multiple choice survey. In addition,
+// you must provide an array of choices. 
 function createMC(sessionId, questionText, getSurvey, choices) {
 	
 	// Encode the choices as a json string
