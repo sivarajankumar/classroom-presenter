0abea15d65121ec24c196c0b6850cc72637fecb2
chriacua <chriacua@gmail.com>
2011-05-19 18:12:02 -0700

    A LINE GRAPH- Woohoo! :D Also, fixed up the ajax request for impending integration, made the graph so that it runs indefinitely (no more start and stop buttons here), fixed title, and added comments

--- Incognito/instr/plotWindow.js
+++ Incognito/instr/plotWindow.js
@@ -1,20 +1,22 @@
- var data1 = []; 
+ 
+ $(document).ready(function() {
+	doTimer();
+});
+
+ var feedPoints = []; //keeps track of all [x,y] points
  var options = {
         lines: { show: true },
         points: { show: true },
         xaxis: { tickDecimals: 0, tickSize: 5 }
     };
-
-
 	
-var c=1;
-var t;
-var currentTime = 0;
+var subtotal = 0;
+var t; //timer
+var currentTime = 0; //used to keep track of where to plot on x-axis
 var timer_is_on=0;
 
 function timedCount(){
-	sendToGetValue(c, currentTime);
-	c=c+1;
+	getActivity(subtotal, currentTime);
 	currentTime = currentTime+5;
 	t=setTimeout("timedCount()",5000);
 }
@@ -28,21 +30,21 @@ function doTimer(){
 }
 
 function stopCount(){
-	alert("got her");
+	//alert(subtotal);
 	clearTimeout(t);
 	timer_is_on=0;
 }
 
-$("#test1").live('click', function(event){
+/*$("#test1").live('click', function(event){
 	doTimer();
 });
 
 $("#test2").live('click', function(event){
 	stopCount();
-});
+});*/
 
 // Function to handle ajax.
-function sendToGetValue(str, curTime){
+function getActivity(str, curTime){
     // get(file, data, callback, type); (only "file" is required)
     $.get(      
     "testingajax.php", //Ajax file
@@ -50,30 +52,20 @@ function sendToGetValue(str, curTime){
 	  time: curTime	},  // create an object will all values
     //function that is called when server returns a value.
     function(data){	
-		data1.push(data);
+		
+		var temp = data[1];
+		data[1] = data[1] - subtotal;
+		subtotal = temp;
+		//alert(subtotal);
+		feedPoints.push(data);
 		var plotarea = $("#graphPage");  
 		plotarea.css("height", "250px");  
 		plotarea.css("width", "250px");  
-		$.plot( plotarea , data1, options );	
+		$.plot( plotarea , [feedPoints], options );	
 		//var obj = jQuery.parseJSON(data);
 		//alert(obj.name
     },
     //How you want the data formated when it is returned from the server.
     "json"
-    );   
-}
-
-// Function to handle ajax.
-function sendValue(str){
-    // get(file, data, callback, type); (only "file" is required)
-    $.get(      
-    "testingajax.php", //Ajax file
-    { d: str },  // create an object will all values
-    //function that is called when server returns a value.
-    function(data){
-        $('#placeHere2').html(data);
-    },
-    //How you want the data formated when it is returned from the server.
-    "html"
     );   
 }
