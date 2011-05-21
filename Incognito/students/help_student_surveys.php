0e10408dcac324b883f5a815ed361ff0bbd17b63
furbyownz <furbyownz@localhost>
2011-05-20 20:49:06 -0700

    Added student help pages

---
+++ Incognito/students/help_student_surveys.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Incognito</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+		<div id="topbanner"> 			
+			<img src="logo.png" alt="logo" />
+		</div>			
+
+		
+		<div id="navigation">	<!-- Navigation bar -->
+			<ul>
+				<li><span><a class="tab" href="studenthome.php">Home</a></span></li>
+				<li><span><a class="tab" href="studentfeed.php">Feed</a></span></li>
+				<li><span><a class="tab" href="studentsurveys.php">Surveys</a></span></li>
+			</ul>		
+		</div>
+		
+		<div id="studenthelpnavigation">
+			<a href="help_questions_feedback.php">Questions and Feedback</a> | 
+		<a href="help_alias.php">Aliases</a> | 
+		<a href="help_student_courses.php">Your Courses</a> |  
+		<a href="help_student_feed.php">Using the Feed</a> | 
+		<a href="help_student_surveys.php">Surveys</a> | 
+		<a href="help_students.php">Troubleshooting</a> 
+		</div>
+
+		
+		<div id="maincontent">
+			<h1>Surveys</h1>Incognito's surveys let instructors create questions 
+			that students can respond to using Incognito. These questions can 
+			either be multiple choice or free response, which lets you type in 
+			any response you want.<br />
+			<h2>Responding To A Survey</h2>1.&nbsp;&nbsp;&nbsp; Go to the Survey 
+			page.<br />
+			2.&nbsp;&nbsp;&nbsp; Click the "Respond" checkbox next to the prompt 
+			you want to answer.<br />
+			3.&nbsp;&nbsp;&nbsp; Type your response into the provided check box.<br />
+			4.&nbsp;&nbsp;&nbsp; Click "Submit."<br />
+		</div>
+
+		
+		
+		<div class="bottomlinks">	<!-- Links at bottom of page -->
+			<a class="aboutlink" href="">Report Bug</a> | 
+			<a class="aboutlink" href="help.php">About</a> | 
+			<a class="aboutlink" href="help.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="help.php">Contact Us</a>
+		</div>
+		
+	</body>
+</html>
