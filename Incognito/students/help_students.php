0c9136770310bd366ea3a85da212772591ee6100
furbyownz <furbyownz@localhost>
2011-05-20 22:43:14 -0700

    Got rid of outdated stuff on student help index page

--- Incognito/students/help_students.php
+++ Incognito/students/help_students.php
@@ -33,63 +33,7 @@
 		</div>
 				
 		<div id = "maincontent">
-			<h1>Student Help Index</h1>
-			<big><strong><br />
-			Enrolling in a Course:</strong></big>	
-		<br />
-			1. Give your CS Net ID to the instructor teaching the course.<br />
-			2. When your instructor has added you to the course, the course will appear in your course list.<br />
-		<br />
-		<big><strong>Viewing Your Courses:</strong></big>
-		<br />
-			1. Go to the "Your Settings" page.<br />
-			2. The list of courses you are enrolled in will appear in your course list.<br />
-		<br />
-		<big><strong>Joining a Course Session:</strong></big>
-		<br />
-			1. Go to the "Your Settings" page.<br />
-			2. Under course list, click "Join" next to the course you want to join.<br />
-		<br />
-		<big><strong>Changing Your Alias:</strong></big>
-		<br />
-			1. Go to the "Your Settings" page.<br />
-			2. Type your new alias into the "Change Alias" text box.<br />
-			3. Click the "Change Alias" button.<br />
-		<br />
-		<big><strong>Submitting a Question:</strong></big>
-		<br />
-			1. Go to either the Home page or the Feed page.<br />
-			2. Type your qestion into the provided text box.<br />
-			3. Select "Question" from the "Submit As:" options above the text box.<br />
-			4. Click "Submit."<br />
-		<br />
-		<big><strong>Submitting Feedback:</strong></big>
-		<br />
-			1. Go to either the Home page or the Feed page.<br />
-			2. Type your feedback into the provided text box.<br />
-			3. Select "Feedback" from the "Submit As:" options above the text box.<br />
-			4. Click "Submit."<br />
-		<br />
-		<big><strong>Responding to a Poll or Free Response Question:</strong></big>
-		<br />
-			1. Go to the Survey page.<br />
-			2. Click the "Respond" checkbox next to the prompt you want to answer.<br />
-			3. Type your response into the provided text box.<br />
-			4. Click "Submit."<br />
-		<br />
-		<big><strong>Voting For an Already Submitted Question or Feedback:</strong></big>
-		<br />
-		<i>Option 1:</i><br />
-			1. Go to the Home page or the Feed page.
-			2. Begin typing your question or comment into the text box. An autocomplete form will display past submissions that match your entry.<br />
-			3. Select the desired submission from the autocomplete form.<br />
-			4. Click "Submit."<br />
-		<br />
-		<i>Option 2:</i><br />
-			1. Go to the Feed page.<br />
-			2. Click the thumbs-up icon next to the question or comment you want to vote for.<br />
-		<br />
-		
+			<h1>Student Help Index</h1>		
 		</div>
 
 		
