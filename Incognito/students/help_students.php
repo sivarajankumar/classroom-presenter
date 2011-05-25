e0cb4e12fd1545f0e4f7bbc848c110426aa68ac0
furbyownz <furbyownz@localhost>
2011-05-25 14:10:45 -0700

    Moved the include db_credentials for answer_question.php inside connectToDB() to fix some scoping issues

---
+++ Incognito/students/help_students.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Incognito</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		
+		<div id="topbanner"> 			
+			<img src="logo.png" alt="logo" />
+		</div>			
+
+		
+		<div id="navigation">	<!-- Navigation bar -->
+			<ul>
+				<li><span><a class="tab" href="studenthome.php">Home</a></span></li>
+				<li><span><a class="tab" href="studentfeed.php">Feed</a></span></li>
+				<li><span><a class="tab" href="studentsurveys.php">Surveys</a></span></li>
+			</ul>		
+		</div>
+		
+		<div id="studenthelpnavigation">
+			<a href="help_questions_feedback.php">Questions and Feedback</a> | 
+		<a href="help_alias.php">Aliases</a> | 
+		<a href="help_student_courses.php">Your Courses</a> |  
+		<a href="help_student_feed.php">Using the Feed</a> | 
+		<a href="help_student_surveys.php">Surveys</a> | 
+		<a href="help_students.php">Troubleshooting</a> 
+		</div>
+				
+		<div id = "maincontent">
+			<h1>Student Help Index</h1>Welcome to Incognito's student help. Use 
+			the links below the navigation bar to select a topic.<br />
+				
+		</div>
+
+		
+		
+		<div class="bottomlinks">	<!-- Links at bottom of page -->
+			<a class="aboutlink" href="">Report Bug</a> | 
+			<a class="aboutlink" href="help.php">About</a> | 
+			<a class="aboutlink" href="help.php">Privacy Policy</a> | 
+			<a class="aboutlink" href="help.php">Contact Us</a>
+		</div>
+		
+	</body>
+</html>
---
+++ Incognito/students/studenthome.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+$id = 1;   
+ include "../doLogin.php";
+include "common_student.php";
+?>
+
+	<head>
+		<title>Incognito</title>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link href="pages.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
+		<script type="text/javascript" src="studentUIcontroller.js"></script>
+		<script type="text/javascript" src="jquery-1.5.2.js"></script>
+		<script type="text/javascript" src="jquery.cookie.js"></script>
+		<script type="text/javascript" src="scripts/StudentSettingsView.js"></script>
+
+	</head>
+
+	<body>
+		
+		<?php //Inserting the banner, greeting, and navigation from common_student.php
+			bannerAndNavigation('Home'); 
+		?>
+		
+		<div id = "maincontent">
+    <form id="submitform">
+				<div class="submissioncontent">	<!-- Includes: "Submit as", textbox, & submit button -->
+					<div id="typeAreaFeed">
+						<span>Submit as:
+							<label><input type="radio" name="submitType" value="Q" checked="checked"/> Question</label>
+							<label><input type="radio" name="submitType" value="F"/> Feedback </label>
+						</span> <br />
+						<input type="textbox" name="texthome" value="" id="ac-input" height="1000" size="80" maxlength="50">
+            <div id="display"></div>
+						<button type="submit" id="submitbutton" onClick="onSubmit()">Submit</button>
+					</div>
+				</div>
+		</div>
+<script>
+YUI({ filter: 'raw' }).use("autocomplete", "autocomplete-filters", "autocomplete-highlighters", function (Y) {
+  Y.one('#ac-input').plug(Y.Plugin.AutoComplete, {
+    resultFilters    : 'phraseMatch',
+    resultHighlighter: 'phraseMatch',
+    source           : function(query) {
+		$.ajax({
+			type: "POST",
+			url: "scripts/studenthome_lookup_questions.php",
+			data: "sid=23456", // still need to retrieve the session ID dynamically.
+			success: function(msg){
+				data = new Array();
+				for (var i = 0; i < msg.length; i++)
+				{
+					//alert( msg[i].text );
+					data[i] = msg[i].text;
+				}
+			}
+		});
+		return data;
+	}
+  });
+});
+</script>
+
+		<?php //Inserting report a bug, about, privacy policy, contact us links
+			bottomLinks();
+		?>
+		
+	</body>
+</html>
