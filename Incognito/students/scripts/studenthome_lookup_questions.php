4547c292f30ab9b3d60dbffbb5ac79279f9b5901
furbyownz <furbyownz@localhost>
2011-05-24 22:32:21 -0700

    Added tests for studenthome_lookup_questions.php

--- Incognito/students/scripts/studenthome_lookup_questions.php
+++ Incognito/students/scripts/studenthome_lookup_questions.php
@@ -1,58 +1,65 @@
 <?php
-	function connectToDB()
+	if (!function_exists('connectToDB'))
 	{
-		// Connect to the database
+		function connectToDB()
+		{
+			// Connect to the database
 
-		include 'db_credentials.php';
+			include 'db_credentials.php';
 
-		$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
-	 
-		if (!$db_conn) {
-			die("Failed to connect to the mysql server"); 
+			$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+		 
+			if (!$db_conn) {
+				die("Failed to connect to the mysql server"); 
+			}
+
+			mysql_select_db($db_name, $db_conn);
+			return $db_conn;
 		}
-
-		mysql_select_db($db_name, $db_conn);
-		return $db_conn;
 	}
 
-	function getQuestions($sid)
+	if (!function_exists('getQuestions'))
 	{
-		// This file looks up all questions associated with a given session. It's designed
-		// for use with the student home page, so its only parameter is the session ID,
-		// and it doesn't support any sorting or filtering of the data.
+		function getQuestions($sid)
+		{
+			// This file looks up all questions associated with a given session. It's designed
+			// for use with the student home page, so its only parameter is the session ID,
+			// and it doesn't support any sorting or filtering of the data.
 
-		$db_conn = connectToDB();
-		
-		// Since we want data for the autocomplete box, we want to get
-		// all questions and feedback in the database.
-		
-		// Query Question and fetch results
-		$query = sprintf("SELECT * FROM Question WHERE sid = %d", $sid);
-		$results = mysql_query($query, $db_conn);
-		$rows = array();
-		while($r = mysql_fetch_assoc($results))
-		{
-			$rows[] = array('text'=>$r["text"],'votes'=>$r["numvotes"],'answered'=>$r["answered"],'type'=>'Q');
+			$db_conn = connectToDB();
+			
+			// Since we want data for the autocomplete box, we want to get
+			// all questions and feedback in the database.
+			
+			// Query Question and fetch results
+			$query = sprintf("SELECT * FROM Question WHERE sid = %d", $sid);
+			$results = mysql_query($query, $db_conn);
+			$rows = array();
+			while($r = mysql_fetch_assoc($results))
+			{
+				$rows[] = array('text'=>$r["text"],'votes'=>$r["numvotes"],'answered'=>$r["answered"],'type'=>'Q');
+			}
+			
+			// Query Feedback and fetch results
+			$query = sprintf("SELECT * FROM Feedback WHERE sid = %d", $sid);
+			$results = mysql_query($query, $db_conn);
+			while($r = mysql_fetch_assoc($results))
+			{
+				$rows[] = array('text'=>$r["text"],'votes'=>$r["numvotes"],'isread'=>$r["isread"],'type'=>'F');
+			}
+			
+			return $rows;
 		}
-		
-		// Query Feedback and fetch results
-		$query = sprintf("SELECT * FROM Feedback WHERE sid = %d", $sid);
-		$results = mysql_query($query, $db_conn);
-		while($r = mysql_fetch_assoc($results))
-		{
-			$rows[] = array('text'=>$r["text"],'votes'=>$r["numvotes"],'isread'=>$r["isread"],'type'=>'F');
-		}
-		
-		// Return the questions and feedback back to the calling Javascript
-		// as a JSON object
-		header('Content-type: application/json');
-		return json_encode($rows);
 	}
 	
 	if (isset($_POST['sid']))
 	{
 		$sid = $_POST['sid'];
 		$results = getQuestions($sid);
-		echo $results;
+		
+		// Return the questions and feedback back to the calling Javascript
+		// as a JSON object
+		header('Content-type: application/json');
+		echo json_encode($results);
 	}
 ?>
