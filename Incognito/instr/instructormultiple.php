375bb0c16b89ad7a8b118342ded019350d047514
chriacua <chriacua@gmail.com>
2011-05-24 19:11:20 -0700

    instructor pages' banner/greeting, navigation bar, bottom links abstracted to common_instructor.php

--- Incognito/instr/instructormultiple.php
+++ Incognito/instr/instructormultiple.php
@@ -1,6 +1,6 @@
 <?php
     include "../doLogin.php";
-	$thisPage='Surveys';
+	include "common_instructor.php";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
@@ -19,25 +19,9 @@
 
 	<body>
 		
-		<div id="topbanner"> <!-- Includes logo & person's information/help/logout, & feed status -->			
-			<a href="instructorfeed.php"><img src="logo.png" alt="logo" /></a>
-			<div id="greeting">
-				<?php echo 'Hello '.($_COOKIE['alias']!='' ? $_COOKIE['alias'] : 'Instructor') ?> 
-				| <a href="instructorsettings.php">Your Settings</a> 
-				| <a class="../help.com" href="help.php">Help</a> 
-				| <a href="scripts/logout.php">Logout</a> <br />
-			</div>
-		</div>		
-
-		
-		<div id="navigation">	<!-- Navigation bar -->
-			<ul>
-				<li <?=($thisPage=='Feed') ? ' id="currentpage"' : ' id="feedTab"' ?>><span><a class="tab" href="instructorfeed.php">Feed</a></span></li>
-				<li <?=($thisPage=='Surveys') ? ' id="currentpage"' : ' id="surveys"' ?>><span><a class="tab" href="instructorfree.php">Surveys</a></span></li>
-				<!--<li><span><a class="tab" href="instructorhistory.php">History</a></span></li>-->
-			</ul>		
-			<a href=""><span id="timeline">VIEW TIMELINE </span></a>
-		</div>
+		<?php //Inserting the banner, greeting, and navigation from common_instructor.php
+			bannerAndNavigation('Surveys'); 
+		?>
 		
 		<div id = "maincontent">
             <form id="multisurvey">
@@ -89,11 +73,9 @@
 
 			
 
-		<div class="bottomlinks">
-			<a class="aboutlink" href="../bugreport.php">Report Bug</a> | 
-			<a class="aboutlink" href="http://code.google.com/p/classroom-presenter/wiki/HomePage">About</a> | 
-			<a class="aboutlink" >Privacy Policy</a> | 
-			<a class="aboutlink" href="mailto:fu11h0use@googlegroups.com">Contact Us</a>
-		</div>
+		<?php //Inserting report a bug, about, privacy policy, contact us links
+			bottomLinks();
+		?>
+		
 	</body>
 </html>
