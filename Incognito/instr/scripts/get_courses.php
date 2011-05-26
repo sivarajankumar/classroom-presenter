2ca5b1e44264e10c25f995346c5ad17bb143ee89
schwenzr <schwenzr@gmail.com>
2011-05-26 07:33:58 -0700

    Changed the query back to the original except with a uid. Also I changed up the logic a bit for when we are iterating through the results. Still testing.

--- Incognito/instr/scripts/get_courses.php
+++ Incognito/instr/scripts/get_courses.php
@@ -13,29 +13,34 @@
 		
 		mysql_select_db($db_name, $db_conn);
 		$uid = $_POST['uid'];
-		// Query the database for all courses that the teacher teaches
-       // $query2 = sprintf("SELECT DISTINCT c.cid, c.name, s.open FROM User u, Teaches t, Course c, Session s WHERE u.email = '%s' AND t.uid = u.uid AND t.cid = c.cid AND s.cid = t.cid;", $email);
-		$query2 = sprintf("SELECT DISTINCT c.cid, c.name FROM Teaches t, Course c WHERE t.uid = %d AND t.cid = c.cid;", $uid);
-		$results2 = mysql_query($query2, $db_conn);
 		
+		// Query the database for all courses that the teacher teaches
+       		$query = sprintf("SELECT DISTINCT c.cid, c.name, s.open FROM User u, Teaches t, Course c, Session s WHERE u.uid = %d AND t.uid = u.uid AND t.cid = c.cid AND s.cid = t.cid;", $uid);
+		
+		$results = mysql_query($query, $db_conn);
+
+		// Error Check
+		if (!$results) {
+			die("Error: " . mysql_error($db_conn)); 
+		}		
+
 		// Then return the results in HTML form
 		$open = 0; 
-		while ($row = mysql_fetch_row($results2)) {
+		while ($row = mysql_fetch_row($results)) {
+			
 			// for each course, also check to see if a session is already open.
-			$query3 = sprintf("Select sid from Session where open = 1 and cid = %d;", $row[0]);
-			$results3 = mysql_query($query3, $db_conn);
 			// if a session row already exists then the session is already open
-			if ($row2 = mysql_fetch_row($results3)) {
+			if ($row[2] == 1) {
 				echo  "<br />$row[1]: <button class='closeOptionButton' id='$row[0]'>Close?</button>
 						Student NetId: <input type='text' id='studentToAdd'/>
-                        <button id='$row[0]' class='addStudentButton'>Add Student</button>";
-						$open = 1;
-			} else if (!$open) {
+                        			<button id='$row[0]' class='addStudentButton'>Add Student</button>";
+				$open = 1;
+			} else if (!$open && $row[2] == 0) {
 				
 				echo  "<br />$row[1]: <button class='openOptionButton' id='$row[0]'>Open?</button>
 						<button id='$row[0]' class='courseRemoveButton'>Delete</button>
 						Student NetId: <input type='text' id='studentToAdd'/>
-                        <button id='$row[0]' class='addStudentButton'>Add Student</button>";
+                        			<button id='$row[0]' class='addStudentButton'>Add Student</button>";
 				
 			} else {
 				
