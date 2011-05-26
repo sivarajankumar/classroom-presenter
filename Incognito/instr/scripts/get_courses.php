4931d2ce95e11b4bed865fac6567d119834001d4
schwenzr <schwenzr@gmail.com>
2011-05-26 08:06:06 -0700

    Changed the create courses so that courses do not create a session when they are made. In addition, I updated get_courses to reflect this change

--- Incognito/instr/scripts/get_courses.php
+++ Incognito/instr/scripts/get_courses.php
@@ -15,7 +15,7 @@
 		$uid = $_POST['uid'];
 		
 		// Query the database for all courses that the teacher teaches
-       		$query = sprintf("SELECT DISTINCT c.cid, c.name, s.open FROM User u, Teaches t, Course c, Session s WHERE u.uid = %d AND t.uid = u.uid AND t.cid = c.cid AND s.cid = t.cid;", $uid);
+       		$query = sprintf("SELECT DISTINCT c.cid, c.name FROM User u, Teaches t, Course c WHERE u.uid = %d AND t.uid = u.uid AND t.cid = c.cid;", $uid);
 		
 		$results = mysql_query($query, $db_conn);
 
@@ -28,14 +28,23 @@
 		$open = 0; 
 		while ($row = mysql_fetch_row($results)) {
 			
+			// See if there is an open session for the course
+			$query = sprintf("SELECT * FROM Session WHERE cid = %d AND open = 1;", $row[0]); 
+			$results1 = mysql_query($query, $db_conn); 
+			
+			// Error check
+			if (!$results1) {
+				die("Error: " . mysql_error($db_conn)); 
+			}
+
 			// for each course, also check to see if a session is already open.
 			// if a session row already exists then the session is already open
-			if ($row[2] == 1) {
+			if (mysql_num_rows($results1) > 0) {
 				echo  "<br />$row[1]: <button class='closeOptionButton' id='$row[0]'>Close?</button>
 						Student NetId: <input type='text' id='studentToAdd'/>
                         			<button id='$row[0]' class='addStudentButton'>Add Student</button>";
 				$open = 1;
-			} else if (!$open && $row[2] == 0) {
+			} else if (!$open) {
 				
 				echo  "<br />$row[1]: <button class='openOptionButton' id='$row[0]'>Open?</button>
 						<button id='$row[0]' class='courseRemoveButton'>Delete</button>
