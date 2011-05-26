f22d60a576e889eda7d46e2eab124682104e3ed4
Mike Kung <lilkung0329@gmail.com>
2011-05-26 10:44:47 -0700

    fixed feed and home refresh

--- Incognito/students/studentsettings.php
+++ Incognito/students/studentsettings.php
@@ -12,9 +12,9 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="pages.css" type="text/css" rel="stylesheet" />
 		<script src="jquery-1.5.2.js" type="text/javascript"></script>
-		<script type="text/javascript" src="studentSettings.js"></script>
 		<script type="text/javascript" src="jquery.cookie.js"></script>
 		<script type="text/javascript" src="scripts/StudentSettingsView.js"></script>
+        <script type="text/javascript" src="studentSettings.js"></script>
 	</head>
 
 	<body>
