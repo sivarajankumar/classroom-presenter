1fdc82607d74a467b29ef5c56b1ce6caf01e9245
Mike Kung <lilkung0329@gmail.com>
2011-05-26 10:44:47 -0700

    fixed feed and home refresh

--- Incognito/students/studentfeed.php
+++ Incognito/students/studentfeed.php
@@ -4,11 +4,11 @@
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<script type="text/javascript" src="studentfeed.js"></script>
-<script type="text/javascript" src="studentUIcontroller.js"></script>
-<script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
 <script src="jquery-1.5.2.js" type="text/javascript"></script>
 <script type="text/javascript" src="jquery.cookie.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
+<script type="text/javascript" src="studentfeed.js"></script>
+<script type="text/javascript" src="studentUIcontroller.js"></script>
 		
 <html xmlns="http://www.w3.org/1999/xhtml">
 
--- Incognito/students/studenthome.php
+++ Incognito/students/studenthome.php
@@ -11,10 +11,10 @@ include "common_student.php";
 		
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link href="pages.css" type="text/css" rel="stylesheet" />
-		<script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
-		<script type="text/javascript" src="studentUIcontroller.js"></script>
 		<script type="text/javascript" src="jquery-1.5.2.js"></script>
 		<script type="text/javascript" src="jquery.cookie.js"></script>
+        <script type="text/javascript" src="http://yui.yahooapis.com/3.3.0/build/yui/yui-min.js"></script>
+        <script type="text/javascript" src="studentUIcontroller.js"></script>
 		<script type="text/javascript" src="scripts/StudentSettingsView.js"></script>
 
 	</head>
