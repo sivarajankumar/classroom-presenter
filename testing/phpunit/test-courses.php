ad8db925c5fa68ac6e3b8b345f1288d9789a6cc0
schwenzr <schwenzr@gmail.com>
2011-05-27 09:04:32 -0700

    Updated the foreign key constrains in the database for the session table and the course table. In addition, I added some testing for opening a session in the test-courses script.

--- testing/phpunit/test-courses.php
+++ testing/phpunit/test-courses.php
@@ -90,8 +90,10 @@
 			return $uids; 
 		}
 		
-		// This function adds a course to the database
-		public function addCourse($courseName, $iuid) {
+		// This function adds a course to the database given a course name,
+		// an instructor uid, and a flag indicating whether the uid is in fact
+		// an instructor
+		public function addCourse($courseName, $iuid, $instructor) {
 			
 			$_POST['uid'] = $iuid;
 			$_POST['name'] = $courseName;
@@ -99,22 +101,37 @@
 			
 			include "../../Incognito/instr/scripts/create_course.php";
 			
-			// Check to make sure that the course was in fact inserted 
-			$db_conn = $this->connectToDatabase();
-			
-			$query = sprintf("SELECT * FROM Course WHERE cid = %d;", $cid);
-			$results = mysql_query($query, $db_conn);
-			
-			// Error check
-			if (!$results) {
-				die ("Error: " . mysql_error($db_conn));
+			// Make sure that we actually got a cid back
+			$num_results = 0;
+			if (isset($cid)) { 
+				
+				// Check to make sure that the course was in fact inserted
+				$db_conn = $this->connectToDatabase();
+					
+
+				$query = sprintf("SELECT * FROM Course WHERE cid = %d;", $cid);
+				$results = mysql_query($query, $db_conn);
+					
+				// Error check
+				if (!$results) {
+					die ("Error: " . mysql_error($db_conn));
+				}
+				
+				$num_results = mysql_num_rows($results);
 			}
 			
-			$this->assertEquals(1, mysql_num_rows($results));
+			// Check if we are do the test on an instructor a student
+			if ($instructor) {
+				$this->assertEquals(1, $num_results);
+			} else {
+				$this->assertEquals(0, $num_results);
+			}
 		}
 		
 		// This function adds a series of courses and returns the array of courses created
-		public function addCourses($iuid) {
+		// given an instructor uid, and given a student id, tests that a student cannot
+		// create a course.
+		public function addCourses($iuid, $suid) {
 			
 			// Define the number of courses we will create
 			$NUM_COURSES = 20;
@@ -122,10 +139,59 @@
 			$courses; 
 			for ($i = 0; $i < $NUM_COURSES; $i++) {
 				$courses[$i] = "" . rand(1000, 9999);
-				$this->addCourse($courses[$i], $iuid);
+				$this->addCourse($courses[$i], $iuid, 1);
 			}
 			
+			// Next we are going to have the student try to add a course
+			$this->addCourses($courses[0], $suid, 0);
+			
 			return $courses; 
+		}
+		
+		// This function adds a student to a course. Like I mentioned before, since
+		// I have other unit tests for the add student script, I will assume that it
+		// is working.
+		public function addStudent($uid, $cid) {
+			$_POST['uid'] = $uid;
+			$_POST['cid'] = $cid; 
+			
+			include "../../Incognito/instr/scripts/addStudent.php";
+		}
+		
+		// This function opens a session for a paritcular course given 
+		// a course id and an instructor id
+		public function openSession($cid, $uid, $instructor) {
+			
+			$_POST['uid'] = $uid; 
+			$_POST['cid'] = $cid; 
+			
+			include "../../Incognito/instr/scripts/start_session.php";
+
+			// First see if we got a result back
+			$num_results = 0; 
+			if (isset($row[0])) {
+				
+				// Then query the database to see if the session is in the db
+				$db_conn = $this->connectToDatabase(); 
+				
+				$sid = $row[0];
+				$query = sprintf("SELECT * FROM Session WHERE sid = %d;", $sid);
+				$results = mysql_query($query, $db_conn);
+				
+				// Error check
+				if (!$results) {
+					die ("Error: " . mysql_error($db_conn));
+				}
+				
+				$num_results = mysql_num_rows($results);
+			}
+			
+			// Now do the asserts
+			if ($instructor) {
+				$this->assertEquals(1, $num_results);
+			} else {
+				$this->assertEquals(0, $num_results);
+			}
 		}
 		
 		// This test function goes through the entire sequence of events for a 
@@ -136,7 +202,20 @@
 			$instructor = "test-inst";
 			$student = "test-stdnt";
 			$uids = $this->init($instructor, $student);
-			$courses = $this->addCourses($uids[0]);
+			$courses = $this->addCourses($uids[0], $uids[1]);
+			
+			// Next we want to add the student to the courses (Note,
+			// I already created a unit test for this script, so I am
+			// going to assume it works)
+			for ($i = 0; $i < sizeof($uids); $i++) {	
+				$this->addStudent($uids[1], $courses[$i]);
+			}
+			
+			// Now that we have added the student to all of the courses we can
+			// open a session for half of the courses
+			for ($i = 0; $i < sizeof($uids; $i++)) {
+				$this->openSession($courses[$i], $uids[0], 1);
+			}
 		}
 	}
 
