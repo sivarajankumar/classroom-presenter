16b91cc8e3ed3611128eb4712ee1bd31dd1ff03f
schwenzr <schwenzr@gmail.com>
2011-05-27 23:44:35 -0700

    Fixed the build xml file to run all of the tests. We had to make the build target dependent on all of the tests.

---
+++ Incognito/instr/common_instructor.php
@@ -0,0 +1,30 @@
+<?php
+	function bannerAndNavigation($thisPage) { ?>
+		<div id="topbanner"> <!-- Includes logo & person's information/help/logout, & feed status -->			
+			<a href="instructorfeed.php"><img src="logo.png" alt="logo" /></a>
+			<div id="greeting">
+				<?php echo 'Hello '.($_COOKIE['alias']!='' ? $_COOKIE['alias'] : 'Instructor') ?> 
+				| <a href="instructorsettings.php">Your Courses</a> 
+				| <a class="../help.com" href="help_instructors.php">Help</a> 
+				| <a href="scripts/logout.php">Logout</a> <br />
+			</div>
+		</div>			
+		
+		<div id="navigation">	<!-- Navigation bar -->
+			<ul>
+				<li <?=($thisPage=='Feed') ? ' id="currentpage"' : ' id="feedTab"' ?>><span><a class="tab" href="instructorfeed.php">Feed</a></span></li>
+				<li <?=($thisPage=='Surveys') ? ' id="currentpage"' : ' id="surveys"' ?>><span><a class="tab" href="instructorfree.php">Surveys</a></span></li>
+			</ul>		
+			<a href=""><span id="timeline">VIEW TIMELINE </span></a>
+		</div>
+	<?php }
+	
+	function bottomLinks() { ?>
+		<div class="bottomlinks">
+			<a class="aboutlink" href="../bugreport.php">Report Bug</a> | 
+			<a class="aboutlink" href="http://code.google.com/p/classroom-presenter/wiki/HomePage">About</a> | 
+			<a class="aboutlink" >Privacy Policy</a> | 
+			<a class="aboutlink" href="mailto:fu11h0use@googlegroups.com">Contact Us</a>
+		</div>
+	<?php }
+?>
---
+++ testing/phpunit/test-submit-vote.php
@@ -0,0 +1,280 @@
+<?php
+	
+	class testSubmitVote extends PHPUnit_Framework_Testcase
+	{
+		public function testAddVoteToQuestion()
+		{
+			// Connect to DB
+			include '../../db_credentials.php';
+			
+			$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+			if ( !$db_conn )
+			{
+				die("Could not connect");
+			}
+			
+			mysql_select_db($db_name, $db_conn);
+			
+			// Wipe the Questions table
+			$query = "DELETE FROM Question";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Insert a test question
+			$query = "INSERT INTO Question (text, numvotes, answered, sid) VALUES ('test', 0, 0, 23456)";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Retrieve the test question's ID
+			$query = "SELECT qid FROM Question WHERE text = 'test'";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$row = mysql_fetch_row($results);
+			$qid = $row[0];
+			
+			$_POST['type'] = 'Q';
+			$_POST['id'] = $qid;
+			$_POST['uid'] = 123;
+			$_POST['vote'] = "true";
+			
+			include '../../Incognito/students/scripts/submit_vote.php';
+			
+			// Check that the vote count is right
+			$query = "SELECT * FROM Question WHERE numvotes = 1";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(1, mysql_num_rows($results));
+			
+			// Check that QuestionVotedOn was updated correctly
+			$query = sprintf("SELECT * FROM QuestionVotedOn WHERE qid = %d AND uid = 123", $qid);
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(1, mysql_num_rows($results));
+		}
+		
+		public function testAddVoteToFeedback()
+		{
+			// Connect to DB
+			include '../../db_credentials.php';
+			
+			$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+			if ( !$db_conn )
+			{
+				die("Could not connect");
+			}
+			
+			mysql_select_db($db_name, $db_conn);
+			
+			// Wipe the Feedback table
+			$query = "DELETE FROM Feedback";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Insert a test comment
+			$query = "INSERT INTO Feedback (text, numvotes, isread, sid) VALUES ('test', 0, 0, 23456)";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Retrieve the test comment's ID
+			$query = "SELECT fid FROM Feedback WHERE text = 'test'";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$row = mysql_fetch_row($results);
+			$fid = $row[0];
+			
+			$_POST['type'] = 'F';
+			$_POST['id'] = $fid;
+			$_POST['uid'] = 123;
+			$_POST['vote'] = "true";
+			
+			include '../../Incognito/students/scripts/submit_vote.php';
+			
+			// Check that the vote count is right
+			$query = "SELECT * FROM Feedback WHERE numvotes = 1";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(1, mysql_num_rows($results));
+			
+			// Check that FeedbackVotedOn was updated correctly
+			$query = sprintf("SELECT * FROM FeedbackVotedOn WHERE fid = %d AND uid = 123", $fid);
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(1, mysql_num_rows($results));
+		}
+		
+		public function testRemoveVoteFromQuestion()
+		{
+			// Connect to DB
+			include '../../db_credentials.php';
+			
+			$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+			if ( !$db_conn )
+			{
+				die("Could not connect");
+			}
+			
+			mysql_select_db($db_name, $db_conn);
+			
+			// Wipe the Questions table
+			$query = "DELETE FROM Question";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Insert a test question
+			$query = "INSERT INTO Question (text, numvotes, answered, sid) VALUES ('test', 1, 0, 23456)";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Retrieve the test question's ID
+			$query = "SELECT qid FROM Question WHERE text = 'test'";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$row = mysql_fetch_row($results);
+			$qid = $row[0];
+			
+			$query = sprintf("INSERT INTO QuestionVotedOn (qid, uid) VALUES (%d, 123)", $qid);
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			$_POST['type'] = 'Q';
+			$_POST['id'] = $qid;
+			$_POST['uid'] = 123;
+			$_POST['vote'] = "false";
+			
+			include '../../Incognito/students/scripts/submit_vote.php';
+			
+			// Check that the vote count is right
+			$query = "SELECT * FROM Question WHERE numvotes = 0";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(1, mysql_num_rows($results));
+			
+			// Check that QuestionVotedOn was updated correctly
+			$query = sprintf("SELECT * FROM QuestionVotedOn WHERE qid = %d AND uid = 123", $qid);
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(0, mysql_num_rows($results));
+		}
+		
+		public function testRemoveVoteFromFeedback()
+		{
+			// Connect to DB
+			include '../../db_credentials.php';
+			
+			$db_conn = mysql_connect("cubist.cs.washington.edu", $username, $password);
+			if ( !$db_conn )
+			{
+				die("Could not connect");
+			}
+			
+			mysql_select_db($db_name, $db_conn);
+			
+			// Wipe the Feedback table
+			$query = "DELETE FROM Feedback";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Insert a test comment
+			$query = "INSERT INTO Feedback (text, numvotes, isread, sid) VALUES ('test', 1, 0, 23456)";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			// Retrieve the test comment's ID
+			$query = "SELECT fid FROM Feedback WHERE text = 'test'";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$row = mysql_fetch_row($results);
+			$fid = $row[0];
+			
+			$query = sprintf("INSERT INTO FeedbackVotedOn (fid, uid) VALUES (%d, 123)", $fid);
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			
+			$_POST['type'] = 'F';
+			$_POST['id'] = $fid;
+			$_POST['uid'] = 123;
+			$_POST['vote'] = "false";
+			
+			include '../../Incognito/students/scripts/submit_vote.php';
+			
+			// Check that the vote count is right
+			$query = "SELECT * FROM Feedback WHERE numvotes = 0";
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(1, mysql_num_rows($results));
+			
+			// Check that FeedbackVotedOn was updated correctly
+			$query = sprintf("SELECT * FROM FeedbackVotedOn WHERE fid = %d AND uid = 123", $fid);
+			$results = mysql_query($query, $db_conn);
+			if (!$results)
+			{
+				die("Error: " . mysql_error($db_conn));
+			}
+			$this->assertEquals(0, mysql_num_rows($results));
+		}
+	}
+
+?>
