2193696a4dd3101d4386d62a4e1dbeef9d260ee7
furbyownz <furbyownz@localhost>
2011-06-01 21:35:05 -0700

    Added a couple comments to answer_question.php

--- Incognito/instr/scripts/answer_question.php
+++ Incognito/instr/scripts/answer_question.php
@@ -1,6 +1,6 @@
 <?php	
 
-	
+	// Connect to the database and return the database connection
 	function connectToDB()
 	{
 		include "../../db_credentials.php";
@@ -23,11 +23,11 @@
 		
 		if ($type == 'Q')
 		{
-			if ($flag == "true")
+			if ($flag == "true")	// set the question to answered
 			{
 				$query = sprintf("UPDATE Question SET answered = 1 WHERE qid = %d", $id);
 			}
-			else
+			else					// set the question to unanswered
 			{
 				$query = sprintf("UPDATE Question SET answered = 0 WHERE qid = %d", $id);
 			}
@@ -37,11 +37,11 @@
 		}
 		elseif ($type == 'F')
 		{
-			if ($flag == "true")
+			if ($flag == "true")	// set the feedback to read
 			{
 				$query = sprintf("UPDATE Feedback SET isread = 1 WHERE fid = %d", $id);
 			}
-			else
+			else					// set the feedback to unread
 			{
 				$query = sprintf("UPDATE Feedback SET isread = 0 WHERE fid = %d", $id);
 			}
